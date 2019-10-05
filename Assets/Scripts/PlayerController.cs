74c8961bfab6ad976409ed0b57938290ed100b7c
Rodrigo Santos Alvares - 51915755 <51915755@animaedu.intranet>
2019-10-04 22:08:28 -0300

    Scene builded and player collider

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 1;
+    
+    private Rigidbody2D _rigidbody;
+
+    private void Awake(){
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update(){
+
+        if (Input.GetMouseButtonDown(0)) {
+            _rigidbody.velocity = Vector2.up * speed;
+        }
+    }
+    
+}
